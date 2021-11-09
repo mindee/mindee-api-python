b5d6a58d8e30578b4ce78201fc3662bc40b044c5
mohamedmindee <mohamed@mindee.co>
2021-11-09 17:54:56 +0100

    fix: :bug: :white-check-mark: re-initialize file cursor in test

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -12,6 +12,11 @@ def test_mpdf_reconstruct_check_n_pages():
     input_obj_3 = Inputs('./tests/data/invoices/invoice_6p.pdf', cut_pdf=True, n_pdf_pages=3)
     input_obj_2 = Inputs('./tests/data/invoices/invoice_6p.pdf', cut_pdf=True, n_pdf_pages=2)
     input_obj_1 = Inputs('./tests/data/invoices/invoice_6p.pdf', cut_pdf=True, n_pdf_pages=1)
+
+    input_obj_3.file_object.seek(0)
+    input_obj_2.file_object.seek(0)
+    input_obj_1.file_object.seek(0)
+
     src_3 = fitz.open(
                 stream=input_obj_3.file_object.read(),
                 filetype="application/pdf",
