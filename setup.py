3609743bfe3f2e5cdec402b3cb3ba59d3179fb2b
Jedylemindee <jeremy@mindee.co>
2021-12-22 14:46:25 +0100

    chg: :bookmark: update setup.py version number to v1.2.3

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     python_requires=">=3.0",
     name=f"{PACKAGE_NAME}",
     description="Mindee API helper library for python",
-    version="v1.2.2",
+    version="v1.2.3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=GIT_URL,
