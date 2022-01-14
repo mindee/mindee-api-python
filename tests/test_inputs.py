35f76baf9f9fafddcc11ecddc11bd05640cce4db
mohamedmindee <jonathan@mindee.co>
2022-01-14 17:11:13 -0500

    chg: :white_check_mark: Updated and added tests for pdfs Inputs

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -1,14 +1,13 @@
 import pytest
 from mindee import Inputs
-import fitz
 
 
-def test_mpdf_reconstruct():
+def test_pdf_reconstruct():
     with pytest.raises(Exception):
         Inputs("./tests/data/invoices/invoice_6p.pdf", cut_pdf=True, n_pdf_pages=4)
 
 
-def test_mpdf_reconstruct_check_n_pages():
+def test_pdf_reconstruct_check_n_pages():
     input_obj_3 = Inputs(
         "./tests/data/invoices/invoice_6p.pdf", cut_pdf=True, n_pdf_pages=3
     )
@@ -24,21 +23,23 @@ def test_mpdf_reconstruct_check_n_pages():
     input_obj_2.file_object.seek(0)
     input_obj_1.file_object.seek(0)
 
-    src_3 = fitz.open(
-        stream=input_obj_3.file_object.read(),
-        filetype="application/pdf",
-        filename="test.pdf",
-    )
-    src_2 = fitz.open(
-        stream=input_obj_2.file_object.read(),
-        filetype="application/pdf",
-        filename="test.pdf",
-    )
-    src_1 = fitz.open(
-        stream=input_obj_1.file_object.read(),
-        filetype="application/pdf",
-        filename="test.pdf",
-    )
-    assert len(src_3) == 3
-    assert len(src_2) == 2
-    assert len(src_1) == 1
+    assert input_obj_3.count_pdf_pages() == 3
+    assert input_obj_2.count_pdf_pages() == 2
+    assert input_obj_1.count_pdf_pages() == 1
+
+
+def test_input_from_stream():
+    with open("./tests/data/invoices/invoice_6p.pdf", "rb") as fp:
+        input_obj_1 = Inputs(fp, input_type="stream", cut_pdf=True, n_pdf_pages=1)
+    assert input_obj_1.count_pdf_pages() == 1
+
+
+def test_pdf_blank_check():
+    with pytest.raises(Exception):
+        Inputs("./tests/data/pdfs/blank.pdf")
+
+    with pytest.raises(Exception):
+        Inputs("./tests/data/pdfs/blank_1.pdf")
+
+    input_not_blank = Inputs("./tests/data/pdfs/not_blank_image_only.pdf")
+    assert input_not_blank.count_pdf_pages() == 1
