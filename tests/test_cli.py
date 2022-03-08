d472bfff1da321548ba917af262c95417dde3316
Ianaré Sévi <ianare@mindee.co>
2022-03-08 11:22:56 +0100

    :bug: fix custom document in CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,7 @@ from argparse import Namespace
 from tests.utils import clear_envvars
 
 from mindee.__main__ import call_endpoint
+from mindee.http import HTTPException
 
 
 @pytest.fixture
@@ -13,7 +14,7 @@ def custom_doc(monkeypatch):
         product_name="custom",
         doc_type="license_plate",
         username="mindee",
-        api_key="",
+        api_key="dummy",
         raise_on_error=True,
         cut_pdf=True,
         input_type="path",
@@ -37,7 +38,7 @@ def ots_doc(monkeypatch):
 
 
 def test_cli_custom_doc(custom_doc):
-    with pytest.raises(RuntimeError):
+    with pytest.raises(HTTPException):
         call_endpoint(custom_doc)
 
 
@@ -46,12 +47,18 @@ def test_cli_invoice(ots_doc):
     ots_doc.invoice_api_key = ""
     with pytest.raises(RuntimeError):
         call_endpoint(ots_doc)
+    ots_doc.invoice_api_key = "dummy"
+    with pytest.raises(HTTPException):
+        call_endpoint(ots_doc)
 
 
 def test_cli_receipt(ots_doc):
     ots_doc.product_name = "receipt"
     ots_doc.receipt_api_key = ""
     with pytest.raises(RuntimeError):
+        call_endpoint(ots_doc)
+    ots_doc.receipt_api_key = "dummy"
+    with pytest.raises(HTTPException):
         call_endpoint(ots_doc)
 
 
@@ -61,10 +68,17 @@ def test_cli_financial_doc(ots_doc):
     ots_doc.receipt_api_key = ""
     with pytest.raises(RuntimeError):
         call_endpoint(ots_doc)
+    ots_doc.invoice_api_key = "dummy"
+    ots_doc.receipt_api_key = "dummy"
+    with pytest.raises(HTTPException):
+        call_endpoint(ots_doc)
 
 
 def test_cli_passport(ots_doc):
     ots_doc.product_name = "passport"
     ots_doc.passport_api_key = ""
     with pytest.raises(RuntimeError):
+        call_endpoint(ots_doc)
+    ots_doc.passport_api_key = "dummy"
+    with pytest.raises(HTTPException):
         call_endpoint(ots_doc)
