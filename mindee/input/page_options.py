6877b590d8eec58e340123f9c1b104db44414b00
Ianaré Sévi <ianare@mindee.co>
2022-10-26 17:07:59 +0200

    :bug: default min_pages to 0

--- mindee/input/page_options.py
+++ mindee/input/page_options.py
@@ -6,11 +6,18 @@ REMOVE = "REMOVE"
 
 class PageOptions(NamedTuple):
     page_indexes: Sequence
-    """Zero-based list of page indexes.
+    """
+    Zero-based list of page indexes.
     A negative index can be used, indicating an offset from the end of the document.
-    [0, -1] represents the fist and last pages of the document."""
+    [0, -1] represents the fist and last pages of the document.
+    """
     operation: str = KEEP_ONLY
-    """Operation to apply on the document, given the ``page_indexes`` specified.
-    `KEEP_ONLY` or `REMOVE`"""
-    on_min_pages: int = 5
-    """Apply the operation only if document has at least this many pages."""
+    """
+    Operation to apply on the document, given the ``page_indexes`` specified.
+    `KEEP_ONLY` or `REMOVE`
+    """
+    on_min_pages: int = 0
+    """
+    Apply the operation only if document has at least this many pages.
+    Default: `0` (apply on all documents)
+    """
