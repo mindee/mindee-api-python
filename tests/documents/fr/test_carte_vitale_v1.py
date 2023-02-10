2f6f69f09e6e207ef687609c50d53d3b3136c973
Ianaré Sévi <ianare@mindee.co>
2023-02-10 18:06:48 +0100

    :sparkles: add support for fr id card v1

---
+++ tests/documents/fr/test_carte_vitale_v1.py
@@ -0,0 +1,50 @@
+import json
+
+import pytest
+
+from mindee.documents.fr.carte_vitale.carte_vitale_v1 import CarteVitaleV1
+
+FR_CARTE_VITALE_DATA_DIR = "./tests/data/fr/carte_vitale"
+
+FILE_PATH_FR_CARTE_VITALE_V1_COMPLETE = (
+    f"{ FR_CARTE_VITALE_DATA_DIR }/response_v1/complete.json"
+)
+FILE_PATH_FR_CARTE_VITALE_V1_EMPTY = (
+    f"{ FR_CARTE_VITALE_DATA_DIR }/response_v1/empty.json"
+)
+
+
+@pytest.fixture
+def carte_vitale_v1_doc() -> CarteVitaleV1:
+    json_data = json.load(open(FILE_PATH_FR_CARTE_VITALE_V1_COMPLETE))
+    return CarteVitaleV1(json_data["document"]["inference"], page_n=None)
+
+
+@pytest.fixture
+def carte_vitale_v1_doc_empty() -> CarteVitaleV1:
+    json_data = json.load(open(FILE_PATH_FR_CARTE_VITALE_V1_EMPTY))
+    return CarteVitaleV1(json_data["document"]["inference"], page_n=None)
+
+
+@pytest.fixture
+def carte_vitale_v1_page0():
+    json_data = json.load(open(FILE_PATH_FR_CARTE_VITALE_V1_COMPLETE))
+    return CarteVitaleV1(json_data["document"]["inference"]["pages"][0], page_n=0)
+
+
+def test_doc_constructor(carte_vitale_v1_doc):
+    reference_str = (
+        open(f"{ FR_CARTE_VITALE_DATA_DIR }/response_v1/doc_to_string.txt")
+        .read()
+        .strip()
+    )
+    assert str(carte_vitale_v1_doc) == reference_str
+
+
+def test_page0_constructor(carte_vitale_v1_page0):
+    reference_str = (
+        open(f"{ FR_CARTE_VITALE_DATA_DIR }/response_v1/page0_to_string.txt")
+        .read()
+        .strip()
+    )
+    assert str(carte_vitale_v1_page0) == reference_str
---
+++ tests/documents/fr/test_id_card_v1.py
@@ -0,0 +1,42 @@
+import json
+
+import pytest
+
+from mindee.documents.fr.id_card.id_card_v1 import IdCardV1
+
+FR_ID_CARD_DATA_DIR = "./tests/data/fr/id_card"
+
+FILE_PATH_FR_ID_CARD_V1_COMPLETE = f"{ FR_ID_CARD_DATA_DIR }/response_v1/complete.json"
+FILE_PATH_FR_ID_CARD_V1_EMPTY = f"{ FR_ID_CARD_DATA_DIR }/response_v1/empty.json"
+
+
+@pytest.fixture
+def id_card_v1_doc() -> IdCardV1:
+    json_data = json.load(open(FILE_PATH_FR_ID_CARD_V1_COMPLETE))
+    return IdCardV1(json_data["document"]["inference"], page_n=None)
+
+
+@pytest.fixture
+def id_card_v1_doc_empty() -> IdCardV1:
+    json_data = json.load(open(FILE_PATH_FR_ID_CARD_V1_EMPTY))
+    return IdCardV1(json_data["document"]["inference"], page_n=None)
+
+
+@pytest.fixture
+def id_card_v1_page0():
+    json_data = json.load(open(FILE_PATH_FR_ID_CARD_V1_COMPLETE))
+    return IdCardV1(json_data["document"]["inference"]["pages"][0], page_n=0)
+
+
+def test_doc_constructor(id_card_v1_doc):
+    reference_str = (
+        open(f"{ FR_ID_CARD_DATA_DIR }/response_v1/doc_to_string.txt").read().strip()
+    )
+    assert str(id_card_v1_doc) == reference_str
+
+
+def test_page0_constructor(id_card_v1_page0):
+    reference_str = (
+        open(f"{ FR_ID_CARD_DATA_DIR }/response_v1/page0_to_string.txt").read().strip()
+    )
+    assert str(id_card_v1_page0) == reference_str
