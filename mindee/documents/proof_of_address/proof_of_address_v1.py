5b19c0b4ffb88bc00c546f1dc475e98992762514
Ianaré Sévi <ianare@mindee.co>
2023-03-10 18:36:08 +0100

    :memo: update proof of address docstrings

--- mindee/documents/proof_of_address/proof_of_address_v1.py
+++ mindee/documents/proof_of_address/proof_of_address_v1.py
@@ -8,24 +8,26 @@ from mindee.fields.text import TextField
 
 
 class ProofOfAddressV1(Document):
+    """Proof of Address v1 prediction results."""
+
     locale: LocaleField
-    """locale information"""
-    date: DateField
-    """ISO date yyyy-mm-dd. Works both for European and US dates."""
-    dates: List[DateField] = []
-    """All extracted ISO date yyyy-mm-dd"""
-    issuer_address: TextField
-    """Address of the document's issuer."""
-    issuer_company_registration: List[CompanyRegistrationField] = []
-    """Generic: VAT NUMBER, TAX ID, COMPANY REGISTRATION NUMBER or country specific."""
+    """The locale detected on the document."""
     issuer_name: TextField
-    """Name of the person or company issuing the document."""
-    recipient_address: TextField
-    """Address of the recipient."""
-    recipient_company_registration: List[CompanyRegistrationField] = []
-    """Generic: VAT NUMBER, TAX ID, COMPANY REGISTRATION NUMBER or country specific."""
+    """The name of the person or company issuing the document."""
+    issuer_company_registration: List[CompanyRegistrationField]
+    """List of company registrations found for the issuer."""
+    issuer_address: TextField
+    """The address of the document's issuer."""
     recipient_name: TextField
-    """Name of the document's recipient."""
+    """The name of the person or company receiving the document."""
+    recipient_company_registration: List[CompanyRegistrationField]
+    """List of company registrations found for the recipient."""
+    recipient_address: TextField
+    """The address of the recipient."""
+    dates: List[DateField]
+    """List of dates found on the document."""
+    date: DateField
+    """The date the document was issued."""
 
     def __init__(
         self,
@@ -34,7 +36,7 @@ class ProofOfAddressV1(Document):
         page_n: Optional[int] = None,
     ):
         """
-        Proof of Address document.
+        Proof of Address v1 prediction results.
 
         :param api_prediction: Raw prediction from HTTP response
         :param input_source: Input object
@@ -55,56 +57,69 @@ class ProofOfAddressV1(Document):
         Build the object from the prediction API JSON.
 
         :param api_prediction: Raw prediction from HTTP response
-        :param page_n: Page number for multi pages pdf input
+        :param page_n: Page number
         """
         self.locale = LocaleField(
-            api_prediction["locale"], value_key="language", page_n=page_n
+            api_prediction["locale"],
+            page_n=page_n,
         )
-        self.date = DateField(api_prediction["date"], page_n=page_n)
-        self.dates = [
-            DateField(tax_prediction, page_n=page_n)
-            for tax_prediction in api_prediction["dates"]
-        ]
-        self.issuer_name = TextField(api_prediction["issuer_name"], page_n=page_n)
-        self.issuer_address = TextField(api_prediction["issuer_address"], page_n=page_n)
+        self.issuer_name = TextField(
+            api_prediction["issuer_name"],
+            page_n=page_n,
+        )
         self.issuer_company_registration = [
-            CompanyRegistrationField(tax_prediction, page_n=page_n)
-            for tax_prediction in api_prediction["issuer_company_registration"]
+            CompanyRegistrationField(prediction, page_n=page_n)
+            for prediction in api_prediction["issuer_company_registration"]
         ]
-        self.recipient_name = TextField(api_prediction["recipient_name"], page_n=page_n)
-        self.recipient_address = TextField(
-            api_prediction["recipient_address"], page_n=page_n
+        self.issuer_address = TextField(
+            api_prediction["issuer_address"],
+            page_n=page_n,
+        )
+        self.recipient_name = TextField(
+            api_prediction["recipient_name"],
+            page_n=page_n,
         )
         self.recipient_company_registration = [
-            CompanyRegistrationField(tax_prediction, page_n=page_n)
-            for tax_prediction in api_prediction["recipient_company_registration"]
+            CompanyRegistrationField(prediction, page_n=page_n)
+            for prediction in api_prediction["recipient_company_registration"]
         ]
+        self.recipient_address = TextField(
+            api_prediction["recipient_address"],
+            page_n=page_n,
+        )
+        self.dates = [
+            DateField(prediction, page_n=page_n)
+            for prediction in api_prediction["dates"]
+        ]
+        self.date = DateField(
+            api_prediction["date"],
+            page_n=page_n,
+        )
 
     def __str__(self) -> str:
-        issuer_company_registrations = "; ".join(
-            [str(n.value) for n in self.issuer_company_registration]
+        issuer_company_registration = f"\n { ' ' * 28 }".join(
+            [str(item) for item in self.issuer_company_registration],
         )
-        recipient_company_registrations = "; ".join(
-            [str(n.value) for n in self.recipient_company_registration]
+        recipient_company_registration = f"\n { ' ' * 31 }".join(
+            [str(item) for item in self.recipient_company_registration],
         )
-        dates = "\n       ".join([str(n.value) for n in self.dates])
+        dates = f"\n { ' ' * 6 }".join(
+            [str(item) for item in self.dates],
+        )
         return clean_out_string(
             "----- Proof of Address V1 -----\n"
             f"Filename: {self.filename or ''}\n"
-            f"Locale: {self.locale}\n"
-            f"Issuer name: {self.issuer_name}\n"
-            f"Issuer Address: {self.issuer_address}\n"
-            f"Issuer Company Registrations: {issuer_company_registrations}\n"
-            f"Recipient name: {self.recipient_name}\n"
-            f"Recipient Address: {self.recipient_address}\n"
-            f"Recipient Company Registrations: {recipient_company_registrations}\n"
-            f"Issuance Date: {self.date}\n"
-            f"Dates: {dates}\n"
+            f"Locale: { self.locale }\n"
+            f"Issuer Name: { self.issuer_name }\n"
+            f"Issuer Company Registrations: { issuer_company_registration }\n"
+            f"Issuer Address: { self.issuer_address }\n"
+            f"Recipient Name: { self.recipient_name }\n"
+            f"Recipient Company Registrations: { recipient_company_registration }\n"
+            f"Recipient Address: { self.recipient_address }\n"
+            f"Dates: { dates }\n"
+            f"Date of Issue: { self.date }\n"
             "----------------------"
         )
-
-    def _checklist(self) -> None:
-        pass
 
 
 TypeProofOfAddressV1 = TypeVar("TypeProofOfAddressV1", bound=ProofOfAddressV1)
--- tests/documents/test_proof_of_address_v1.py
+++ tests/documents/test_proof_of_address_v1.py
@@ -3,66 +3,42 @@ import json
 import pytest
 
 from mindee.documents.proof_of_address.proof_of_address_v1 import ProofOfAddressV1
-from tests import PROOF_OF_ADDRESS_DATA_DIR
+
+PROOF_OF_ADDRESS_DATA_DIR = "./tests/data/proof_of_address"
 
 FILE_PATH_PROOF_OF_ADDRESS_V1_COMPLETE = (
-    f"{PROOF_OF_ADDRESS_DATA_DIR}/response_v1/complete.json"
+    f"{ PROOF_OF_ADDRESS_DATA_DIR }/response_v1/complete.json"
+)
+FILE_PATH_PROOF_OF_ADDRESS_V1_EMPTY = (
+    f"{ PROOF_OF_ADDRESS_DATA_DIR }/response_v1/empty.json"
 )
 
 
 @pytest.fixture
-def proof_of_address_v1_doc_object() -> ProofOfAddressV1:
+def proof_of_address_v1_doc() -> ProofOfAddressV1:
     json_data = json.load(open(FILE_PATH_PROOF_OF_ADDRESS_V1_COMPLETE))
-    return ProofOfAddressV1(
-        api_prediction=json_data["document"]["inference"], page_n=None
-    )
+    return ProofOfAddressV1(json_data["document"]["inference"], page_n=None)
 
 
 @pytest.fixture
-def proof_of_address_v1_doc_object_empty() -> ProofOfAddressV1:
-    json_data = json.load(open(f"{PROOF_OF_ADDRESS_DATA_DIR}/response_v1/empty.json"))
-    return ProofOfAddressV1(
-        api_prediction=json_data["document"]["inference"], page_n=None
-    )
+def proof_of_address_v1_doc_empty() -> ProofOfAddressV1:
+    json_data = json.load(open(FILE_PATH_PROOF_OF_ADDRESS_V1_EMPTY))
+    return ProofOfAddressV1(json_data["document"]["inference"], page_n=None)
 
 
 @pytest.fixture
-def proof_of_address_v1_page_object() -> ProofOfAddressV1:
+def proof_of_address_v1_page0():
     json_data = json.load(open(FILE_PATH_PROOF_OF_ADDRESS_V1_COMPLETE))
-    return ProofOfAddressV1(
-        api_prediction=json_data["document"]["inference"]["pages"][0], page_n=0
-    )
+    return ProofOfAddressV1(json_data["document"]["inference"]["pages"][0], page_n=0)
 
 
-def test_doc_constructor(proof_of_address_v1_doc_object):
-    doc_str = (
-        open(f"{PROOF_OF_ADDRESS_DATA_DIR}/response_v1/doc_to_string.txt")
-        .read()
-        .strip()
-    )
-    assert proof_of_address_v1_doc_object.issuer_name.page_n == 0
-    assert str(proof_of_address_v1_doc_object) == doc_str
+def test_doc_constructor(proof_of_address_v1_doc):
+    file_path = f"{ PROOF_OF_ADDRESS_DATA_DIR }/response_v1/doc_to_string.txt"
+    reference_str = open(file_path, "r", encoding="utf-8").read().strip()
+    assert str(proof_of_address_v1_doc) == reference_str
 
 
-def test_page_constructor(proof_of_address_v1_page_object):
-    doc_str = (
-        open(f"{PROOF_OF_ADDRESS_DATA_DIR}/response_v1/page0_to_string.txt")
-        .read()
-        .strip()
-    )
-    assert proof_of_address_v1_page_object.orientation.value == 0
-    assert proof_of_address_v1_page_object.issuer_name.page_n == 0
-    assert str(proof_of_address_v1_page_object) == doc_str
-    assert len(proof_of_address_v1_page_object.cropper) == 0
-
-
-def test_all_na(proof_of_address_v1_doc_object_empty):
-    assert proof_of_address_v1_doc_object_empty.locale.value is None
-    assert proof_of_address_v1_doc_object_empty.date.value is None
-    assert len(proof_of_address_v1_doc_object_empty.dates) == 0
-    assert proof_of_address_v1_doc_object_empty.issuer_address.value is None
-    assert len(proof_of_address_v1_doc_object_empty.issuer_company_registration) == 0
-    assert proof_of_address_v1_doc_object_empty.issuer_name.value is None
-    assert proof_of_address_v1_doc_object_empty.recipient_address.value is None
-    assert len(proof_of_address_v1_doc_object_empty.recipient_company_registration) == 0
-    assert proof_of_address_v1_doc_object_empty.recipient_name.value is None
+def test_page0_constructor(proof_of_address_v1_page0):
+    file_path = f"{ PROOF_OF_ADDRESS_DATA_DIR }/response_v1/page0_to_string.txt"
+    reference_str = open(file_path, "r", encoding="utf-8").read().strip()
+    assert str(proof_of_address_v1_page0) == reference_str
