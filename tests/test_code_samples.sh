04887ed3f07072a6383394e2a62d30d3dc9249f2
ianaré <97107275+ianardee@users.noreply.github.com>
2023-05-02 16:56:18 +0200

    :white_check_mark: add default code sample (#129)

--- tests/test_code_samples.sh
+++ tests/test_code_samples.sh
@@ -14,7 +14,7 @@ do
   echo "###############################################"
   echo
 
-  sed "s/my-api-key/${API_KEY}/" "$f" > $OUTPUT_FILE
+  sed "s/my-api-key/${API_KEY}/" "${f}" > $OUTPUT_FILE
   sed -i 's/\/path\/to\/the\/file.ext/.\/tests\/data\/pdf\/blank_1.pdf/' $OUTPUT_FILE
 
   if echo "$f" | grep -q "custom_v1.txt"
@@ -22,5 +22,13 @@ do
     sed -i "s/my-account/$ACCOUNT/g" $OUTPUT_FILE
     sed -i "s/my-endpoint/$ENDPOINT/g" $OUTPUT_FILE
   fi
+
+  if echo "${f}" | grep -q "default.txt"
+  then
+    sed -i "s/my-account/$ACCOUNT/" $OUTPUT_FILE
+    sed -i "s/my-endpoint/$ENDPOINT/" $OUTPUT_FILE
+    sed -i "s/my-version/1/" $OUTPUT_FILE
+  fi
+
   python $OUTPUT_FILE
 done
