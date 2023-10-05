3c518d12a72bd9ca1525adf047dbb2b871e16e93
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-10-05 15:08:22 +0200

    :recycle: fixed client tests

--- mindee/parsing/common/api_response.py
+++ mindee/parsing/common/api_response.py
@@ -1,29 +1,21 @@
 from abc import ABC
-from typing import Generic, Optional
 
 from mindee.logger import logger
 from mindee.parsing.common.api_request import ApiRequest
-from mindee.parsing.common.document import Document
-from mindee.parsing.common.inference import TypeInference
 from mindee.parsing.common.string_dict import StringDict
 
 
-class ApiResponse(ABC, Generic[TypeInference]):
+class ApiResponse(ABC):
     """
     Base class for responses sent by the server.
 
     Serves as a base class for responses to both synchronous and asynchronous calls.
     """
-    
-    document: Optional[Document]
-    """The document object, properly parsed after being retrieved from the server."""
 
     raw_http: StringDict
     """Raw request sent by the server, as string."""
 
-    def __init__(self, prediction_type, raw_response: StringDict) -> None:
+    def __init__(self, raw_response: StringDict) -> None:
         logger.debug("Handling API response")
         self.api_request = ApiRequest(raw_response["api_request"])
         self.raw_http = raw_response
-        if "document" in raw_response and raw_response["document"]:
-            self.document = Document(prediction_type, raw_response["document"])
