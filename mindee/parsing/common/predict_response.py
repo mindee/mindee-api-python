514bacf934cb9abfabe84870bbe82c7600a70ab2
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-10-05 14:42:22 +0200

    :recycle: update client tests & tweak syntax

--- mindee/parsing/common/predict_response.py
+++ mindee/parsing/common/predict_response.py
@@ -1,19 +1,15 @@
 from typing import Generic
 
 from mindee.parsing.common.api_response import ApiResponse, StringDict
-from mindee.parsing.common.document import Document
 from mindee.parsing.common.inference import TypeInference
 
 
-class PredictResponse(Generic[TypeInference], ApiResponse):
+class PredictResponse(ApiResponse[TypeInference]):
     """
     Response of a prediction request.
 
     This is a generic class, so certain class properties depend on the document type.
     """
-
-    document: Document
-    """The document object, properly parsed after being retrieved from the server."""
 
     def __init__(self, prediction_type, raw_response: StringDict) -> None:
         """
@@ -22,5 +18,4 @@ class PredictResponse(Generic[TypeInference], ApiResponse):
         :param input_source: Input object
         :param http_response: json response from HTTP call
         """
-        super().__init__(raw_response)
-        self.document = Document(prediction_type, raw_response["document"])
+        super().__init__(prediction_type, raw_response)
--- tests/test_client.py
+++ tests/test_client.py
@@ -2,36 +2,31 @@ import pytest
 
 from mindee import Client, PageOptions, product
 from mindee.http.error import HTTPException
+from mindee.parsing.common.predict_response import PredictResponse
 from tests import INVOICE_DATA_DIR
 from tests.test_inputs import FILE_TYPES_DIR, PDF_DATA_DIR
 from tests.utils import clear_envvars, dummy_envvars
 
 
 @pytest.fixture
-def empty_client(monkeypatch):
+def empty_client(monkeypatch) -> Client:
     clear_envvars(monkeypatch)
     return Client()
 
 
 @pytest.fixture
-def env_client(monkeypatch):
+def env_client(monkeypatch) -> Client:
     dummy_envvars(monkeypatch)
-    return Client("dummy").create_endpoint(
-        endpoint_name="dummy",
-        account_name="dummy",
-    )
+    return Client("dummy")
 
 
 @pytest.fixture
-def dummy_client():
-    return Client("dummy").create_endpoint(
-        endpoint_name="dummy",
-        account_name="dummy",
-    )
+def dummy_client() -> Client:
+    return Client("dummy")
 
 
 @pytest.fixture
-def dummy_client_no_raise():
+def dummy_client_no_raise() -> Client:
     return Client(api_key="dummy", raise_on_error=False)
 
 
@@ -39,86 +34,30 @@ def test_parse_path_without_token(empty_client: Client):
     with pytest.raises(RuntimeError):
         input_doc = empty_client.source_from_path(f"{PDF_DATA_DIR}/blank.pdf")
         empty_client.parse(product.ReceiptV4, input_doc)
-    # with pytest.raises(RuntimeError):
-    #     empty_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-    #         product.TypeInvoiceV4
-    #     )
-    # with pytest.raises(RuntimeError):
-    #     empty_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-    #         product.TypeFinancialDocumentV1
-    #     )
-    # with pytest.raises(RuntimeError):
-    #     empty_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-    #         product.TypePassportV1
-    #     )
 
 
-# def test_parse_path_with_env_token(env_client):
-# with pytest.raises(HTTPException):
-#     env_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypeReceiptV5
-#     )
-# with pytest.raises(HTTPException):
-#     env_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypeInvoiceV4
-#     )
-# with pytest.raises(HTTPException):
-#     env_client.doc_from_path(
-#         f"{FILE_TYPES_DIR}/receipt.jpg",
-#     ).parse(product.TypeFinancialDocumentV1)
-# with pytest.raises(HTTPException):
-#     env_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypePassportV1
-#     )
-# with pytest.raises(HTTPException):
-#     env_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypeCustomV1, "dummy"
-#     )
+def test_parse_path_with_env_token(env_client: Client):
+    with pytest.raises(HTTPException):
+        input_doc = env_client.source_from_path(f"{PDF_DATA_DIR}/blank.pdf")
+        env_client.parse(product.ReceiptV4, input_doc)
 
 
-# def test_parse_path_with_wrong_filetype(dummy_client):
-# with pytest.raises(AssertionError):
-#     dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpga").parse(
-#         product.TypeReceiptV5
-#     )
-# with pytest.raises(AssertionError):
-#     dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpga").parse(
-#         product.TypeInvoiceV4
-#     )
-# with pytest.raises(AssertionError):
-#     dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpga").parse(
-#         product.TypePassportV1
-#     )
+def test_parse_path_with_wrong_filetype(dummy_client: Client):
+    with pytest.raises(AssertionError):
+        dummy_client.source_from_path(f"{FILE_TYPES_DIR}/receipt.jpga")
 
 
-# def test_parse_path_with_wrong_token(dummy_client):
-# with pytest.raises(HTTPException):
-#     dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypeReceiptV5
-#     )
-# with pytest.raises(HTTPException):
-#     dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypeInvoiceV4
-#     )
-# with pytest.raises(HTTPException):
-#     dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypePassportV1
-#     )
+def test_parse_path_with_wrong_token(dummy_client: Client):
+    with pytest.raises(HTTPException):
+        input_doc = dummy_client.source_from_path(f"{PDF_DATA_DIR}/blank.pdf")
+        dummy_client.parse(product.ReceiptV4, input_doc)
 
 
-# def test_request_with_filepath(dummy_client):
-#     with pytest.raises(HTTPException):
-#         dummy_client.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#             product.TypeReceiptV5
-#         )
-
-
-# def test_request_without_raise_on_error(dummy_client_no_raise):
-#     result = dummy_client_no_raise.doc_from_path(f"{FILE_TYPES_DIR}/receipt.jpg").parse(
-#         product.TypeReceiptV5
-#     )
-#     assert result.document is None
-#     assert len(result.pages) == 0
+def test_request_without_raise_on_error(dummy_client_no_raise: Client):
+    input_doc = dummy_client_no_raise.source_from_path(f"{PDF_DATA_DIR}/blank.pdf")
+    result: PredictResponse = dummy_client_no_raise.parse(product.ReceiptV4, input_doc)
+    with pytest.raises(AttributeError):
+        result.document
 
 
 # def test_request_without_raise_on_error_include_words(dummy_client_no_raise):
