6a467c80ceae95c1a6a191924636eec66e435559
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-10-06 19:03:52 +0200

    fix docstring

--- mindee/product/fr/carte_vitale/carte_vitale_v1_document.py
+++ mindee/product/fr/carte_vitale/carte_vitale_v1_document.py
@@ -26,7 +26,6 @@ class CarteVitaleV1Document(Prediction):
 
         :param raw_prediction: Raw prediction from HTTP response
         """
-
         self.given_names = [
             StringField(prediction, page_id=page_id)
             for prediction in raw_prediction["given_names"]
