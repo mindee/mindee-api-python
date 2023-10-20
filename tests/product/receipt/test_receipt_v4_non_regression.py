98da7e05bcdeef7c8a55dec044d41d50704976d4
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2023-10-20 16:54:09 +0200

    :wrench: add non-regression testing, testing lib update pending

---
+++ tests/product/receipt/test_receipt_v4_non_regression.py
@@ -0,0 +1,22 @@
+from mindee.client import Client
+
+from mindee.product import ReceiptV4
+from tests.product import PRODUCT_DATA_DIR, get_id, get_version
+
+
+def test_default_sample():
+    client = Client()
+    with open(
+        PRODUCT_DATA_DIR / "expense_receipts" / "response_v4" / "default_sample.rst",
+        encoding="utf-8",
+    ) as rst_file:
+        rst_ref = rst_file.read()
+
+    sample = client.source_from_path(
+        PRODUCT_DATA_DIR / "expense_receipts" / "default_sample.jpg"
+    )
+    response = client.parse(ReceiptV4, sample)
+    doc_response = response.document
+    doc_response.id = get_id(rst_ref)
+    doc_response.inference.product.version = get_version(rst_ref)
+    assert str(doc_response) == rst_ref
---
+++ tests/product/us/bank_check/test_banck_check_v1_non_regression.py
@@ -0,0 +1,22 @@
+from mindee.client import Client
+
+from mindee.product.us import BankCheckV1
+from tests.product import PRODUCT_DATA_DIR, get_id, get_version
+
+
+def test_default_sample():
+    client = Client()
+    with open(
+        PRODUCT_DATA_DIR / "bank_check" / "response_v1" / "default_sample.rst",
+        encoding="utf-8",
+    ) as rst_file:
+        rst_ref = rst_file.read()
+
+    sample = client.source_from_path(
+        PRODUCT_DATA_DIR / "bank_check" / "default_sample.jpg"
+    )
+    response = client.parse(BankCheckV1, sample)
+    doc_response = response.document
+    doc_response.id = get_id(rst_ref)
+    doc_response.inference.product.version = get_version(rst_ref)
+    assert str(doc_response) == rst_ref
