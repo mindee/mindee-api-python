379b92d44d34cc1db80f3f7c3c883a100e241a33
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2024-07-19 11:09:02 +0200

    remove broken product

--- mindee/product/us/__init__.py
+++ mindee/product/us/__init__.py
@@ -6,6 +6,13 @@ from mindee.product.us.driver_license.driver_license_v1_document import (
     DriverLicenseV1Document,
 )
 from mindee.product.us.driver_license.driver_license_v1_page import DriverLicenseV1Page
+from mindee.product.us.healthcare_card.healthcare_card_v1 import HealthcareCardV1
+from mindee.product.us.healthcare_card.healthcare_card_v1_copay import (
+    HealthcareCardV1Copay,
+)
+from mindee.product.us.healthcare_card.healthcare_card_v1_document import (
+    HealthcareCardV1Document,
+)
 from mindee.product.us.w9.w9_v1 import W9V1
 from mindee.product.us.w9.w9_v1_document import W9V1Document
 from mindee.product.us.w9.w9_v1_page import W9V1Page
--- mindee/product/us/us_mail/__init__.py
+++
@@ -1,6 +0,0 @@
-from mindee.product.us.us_mail.us_mail_v2 import UsMailV2
-from mindee.product.us.us_mail.us_mail_v2_document import UsMailV2Document
-from mindee.product.us.us_mail.us_mail_v2_recipient_address import (
-    UsMailV2RecipientAddress,
-)
-from mindee.product.us.us_mail.us_mail_v2_sender_address import UsMailV2SenderAddress
