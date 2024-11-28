1f70d469594245e849b2f1778d244b5651dc9589
ianaré <97107275+ianardee@users.noreply.github.com>
2024-11-28 15:31:24 +0100

    :arrow_up: update pylint (#285)

--- mindee/versions.py
+++ mindee/versions.py
@@ -2,7 +2,7 @@ import sys
 
 __version__ = "4.15.0"
 
-python_version = f"{sys.version_info[0]}.{sys.version_info[1]}"
+PYTHON_VERSION = f"{sys.version_info[0]}.{sys.version_info[1]}"
 
 
 def get_platform() -> str:
