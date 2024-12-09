90bdd2c83b0305213aa7d2022e6d334d430f19d4
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2024-12-09 10:40:27 +0100

    :recycle: split up sources module to accomodate for future additions (#288)

---
+++ mindee/input/sources/base_64_input.py
@@ -0,0 +1,20 @@
+import base64
+import io
+
+from mindee.input.sources.local_input_source import InputType, LocalInputSource
+
+
+class Base64Input(LocalInputSource):
+    """Base64-encoded text input."""
+
+    def __init__(self, base64_string: str, filename: str) -> None:
+        """
+        Input document from a base64 encoded string.
+
+        :param base64_string: Raw data as a base64 encoded string
+        :param filename: File name of the input
+        """
+        self.file_object = io.BytesIO(base64.standard_b64decode(base64_string))
+        self.filename = filename
+        self.filepath = None
+        super().__init__(input_type=InputType.BASE64)
---
+++ mindee/input/sources/bytes_input.py
@@ -0,0 +1,19 @@
+import io
+
+from mindee.input.sources.local_input_source import InputType, LocalInputSource
+
+
+class BytesInput(LocalInputSource):
+    """Raw bytes input."""
+
+    def __init__(self, raw_bytes: bytes, filename: str) -> None:
+        """
+        Input document from raw bytes (no buffer).
+
+        :param raw_bytes: Raw data as bytes
+        :param filename: File name of the input
+        """
+        self.file_object = io.BytesIO(raw_bytes)
+        self.filename = filename
+        self.filepath = None
+        super().__init__(input_type=InputType.BYTES)
