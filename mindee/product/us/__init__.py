998617bb919852e7fd2f0e2efa0cdadbb89aad3b
sebastianMindee <sebastian.oliverasilvera@mindee.co>
2025-03-14 15:33:55 +0100

    restore usmail v2

--- mindee/product/us/__init__.py
+++ mindee/product/us/__init__.py
@@ -12,6 +12,16 @@ from mindee.product.us.healthcare_card.healthcare_card_v1_copay import (
 from mindee.product.us.healthcare_card.healthcare_card_v1_document import (
     HealthcareCardV1Document,
 )
+from mindee.product.us.us_mail.us_mail_v2 import UsMailV2
+from mindee.product.us.us_mail.us_mail_v2_document import (
+    UsMailV2Document,
+)
+from mindee.product.us.us_mail.us_mail_v2_recipient_address import (
+    UsMailV2RecipientAddress,
+)
+from mindee.product.us.us_mail.us_mail_v2_sender_address import (
+    UsMailV2SenderAddress,
+)
 from mindee.product.us.us_mail.us_mail_v3 import UsMailV3
 from mindee.product.us.us_mail.us_mail_v3_document import (
     UsMailV3Document,
