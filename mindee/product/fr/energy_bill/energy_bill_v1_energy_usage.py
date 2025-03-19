3d3b68ad1072c5fac2cfae7291b82631984a10a1
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-03-19 17:59:00 +0100

    :sparkles: bump FR EnergyBillV1 to V1.2 & US HealthcareCardV1 to V1.1 (#308)

--- mindee/product/fr/energy_bill/energy_bill_v1_energy_usage.py
+++ mindee/product/fr/energy_bill/energy_bill_v1_energy_usage.py
@@ -13,6 +13,8 @@ from mindee.parsing.standard.base import (
 class EnergyBillV1EnergyUsage(FieldPositionMixin, FieldConfidenceMixin):
     """Details of energy consumption."""
 
+    consumption: Optional[float]
+    """The price per unit of energy consumed."""
     description: Optional[str]
     """Description or details of the energy usage."""
     end_date: Optional[str]
@@ -23,6 +25,8 @@ class EnergyBillV1EnergyUsage(FieldPositionMixin, FieldConfidenceMixin):
     """The rate of tax applied to the total cost."""
     total: Optional[float]
     """The total cost of energy consumed."""
+    unit: Optional[str]
+    """The unit of measurement for energy consumption."""
     unit_price: Optional[float]
     """The price per unit of energy consumed."""
     page_n: int
@@ -44,53 +48,63 @@ class EnergyBillV1EnergyUsage(FieldPositionMixin, FieldConfidenceMixin):
         else:
             self.page_n = page_id
 
+        self.consumption = to_opt_float(raw_prediction, "consumption")
         self.description = raw_prediction["description"]
         self.end_date = raw_prediction["end_date"]
         self.start_date = raw_prediction["start_date"]
         self.tax_rate = to_opt_float(raw_prediction, "tax_rate")
         self.total = to_opt_float(raw_prediction, "total")
+        self.unit = raw_prediction["unit"]
         self.unit_price = to_opt_float(raw_prediction, "unit_price")
 
     def _printable_values(self) -> Dict[str, str]:
         """Return values for printing."""
         out_dict: Dict[str, str] = {}
+        out_dict["consumption"] = float_to_string(self.consumption)
         out_dict["description"] = format_for_display(self.description)
         out_dict["end_date"] = format_for_display(self.end_date)
         out_dict["start_date"] = format_for_display(self.start_date)
         out_dict["tax_rate"] = float_to_string(self.tax_rate)
         out_dict["total"] = float_to_string(self.total)
+        out_dict["unit"] = format_for_display(self.unit)
         out_dict["unit_price"] = float_to_string(self.unit_price)
         return out_dict
 
     def _table_printable_values(self) -> Dict[str, str]:
         """Return values for printing inside an RST table."""
         out_dict: Dict[str, str] = {}
+        out_dict["consumption"] = float_to_string(self.consumption)
         out_dict["description"] = format_for_display(self.description, 36)
         out_dict["end_date"] = format_for_display(self.end_date, 10)
         out_dict["start_date"] = format_for_display(self.start_date, None)
         out_dict["tax_rate"] = float_to_string(self.tax_rate)
         out_dict["total"] = float_to_string(self.total)
+        out_dict["unit"] = format_for_display(self.unit, None)
         out_dict["unit_price"] = float_to_string(self.unit_price)
         return out_dict
 
     def to_table_line(self) -> str:
         """Output in a format suitable for inclusion in an rST table."""
         printable = self._table_printable_values()
-        out_str: str = f"| {printable['description']:<36} | "
+        out_str: str = f"| {printable['consumption']:<11} | "
+        out_str += f"{printable['description']:<36} | "
         out_str += f"{printable['end_date']:<10} | "
         out_str += f"{printable['start_date']:<10} | "
         out_str += f"{printable['tax_rate']:<8} | "
         out_str += f"{printable['total']:<9} | "
+        out_str += f"{printable['unit']:<15} | "
         out_str += f"{printable['unit_price']:<10} | "
         return clean_out_string(out_str)
 
     def __str__(self) -> str:
         """Default string representation."""
         printable = self._printable_values()
-        out_str: str = f"Description: {printable['description']}, \n"
+        out_str: str = f"Consumption: {printable['consumption']}, \n"
+        out_str += f"Description: {printable['description']}, \n"
         out_str += f"End Date: {printable['end_date']}, \n"
         out_str += f"Start Date: {printable['start_date']}, \n"
         out_str += f"Tax Rate: {printable['tax_rate']}, \n"
         out_str += f"Total: {printable['total']}, \n"
+        out_str += f"Unit of Measure: {printable['unit']}, \n"
         out_str += f"Unit Price: {printable['unit_price']}, \n"
         return clean_out_string(out_str)
