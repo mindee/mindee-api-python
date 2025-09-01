13834be96de4fe3eedcaf482670e1f436974888f
ianaré <97107275+ianardee@users.noreply.github.com>
2025-09-01 18:01:29 +0200

    :sparkles: add list field typed accessors (#353)

--- mindee/parsing/v2/field/list_field.py
+++ mindee/parsing/v2/field/list_field.py
@@ -7,6 +7,8 @@ from mindee.parsing.v2.field.dynamic_field import (
     FieldType,
     get_field_type,
 )
+from mindee.parsing.v2.field.object_field import ObjectField
+from mindee.parsing.v2.field.simple_field import SimpleField
 
 
 class ListField(BaseField):
@@ -21,6 +23,28 @@ class ListField(BaseField):
         self.items = []
         for item in raw_response["items"]:
             self.items.append(get_field_type(item))
+
+    @property
+    def simple_items(self) -> List[SimpleField]:
+        """List of items as ``SimpleField``."""
+        simple_items = []
+        for item in self.items:
+            if isinstance(item, SimpleField):
+                simple_items.append(item)
+            else:
+                raise ValueError("List item is not a simple field.")
+        return simple_items
+
+    @property
+    def object_items(self) -> List[ObjectField]:
+        """List of items as ``ObjectField``."""
+        object_items = []
+        for item in self.items:
+            if isinstance(item, ObjectField):
+                object_items.append(item)
+            else:
+                raise ValueError("List item is not an object field.")
+        return object_items
 
     def __str__(self) -> str:
         out_str = ""
