c0f45a79185fc6162d1c811c3c7ad71f7fa91140
ianaré <97107275+ianardee@users.noreply.github.com>
2025-09-26 14:03:54 +0200

    :coffin: remove obsolete US Mail V2 (#361)

--- mindee/product/us/us_mail/__init__.py
+++ mindee/product/us/us_mail/__init__.py
@@ -1,13 +1,3 @@
-from mindee.product.us.us_mail.us_mail_v2 import UsMailV2
-from mindee.product.us.us_mail.us_mail_v2_document import (
-    UsMailV2Document,
-)
-from mindee.product.us.us_mail.us_mail_v2_recipient_address import (
-    UsMailV2RecipientAddress,
-)
-from mindee.product.us.us_mail.us_mail_v2_sender_address import (
-    UsMailV2SenderAddress,
-)
 from mindee.product.us.us_mail.us_mail_v3 import UsMailV3
 from mindee.product.us.us_mail.us_mail_v3_document import (
     UsMailV3Document,
