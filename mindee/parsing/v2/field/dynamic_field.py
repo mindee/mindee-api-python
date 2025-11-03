b496bbcad13bf95c8541965feb9c16d80443f38f
Ianaré Sévi <ianare@mindee.co>
2025-11-03 16:34:08 +0100

    :label: better field types

--- mindee/parsing/v2/field/dynamic_field.py
+++ mindee/parsing/v2/field/dynamic_field.py
@@ -1,8 +1,14 @@
 from enum import Enum
 from importlib import import_module
+from typing import TYPE_CHECKING, Union
 
 from mindee.error import MindeeApiV2Error
 from mindee.parsing.common.string_dict import StringDict
+
+if TYPE_CHECKING:
+    from mindee.parsing.v2.field.list_field import ListField
+    from mindee.parsing.v2.field.object_field import ObjectField
+    from mindee.parsing.v2.field.simple_field import SimpleField
 
 
 class FieldType(str, Enum):
@@ -11,6 +17,9 @@ class FieldType(str, Enum):
     OBJECT = "ObjectField"
     LIST = "ListField"
     SIMPLE = "SimpleField"
+
+
+FieldTypeAlias = Union["SimpleField", "ListField", "ObjectField"]
 
 
 class DynamicField:
@@ -30,7 +39,10 @@ class DynamicField:
         return str(self)
 
 
-def get_field_type(raw_response: StringDict, indent_level: int = 0) -> DynamicField:
+def get_field_type(
+    raw_response: StringDict,
+    indent_level: int = 0,
+) -> FieldTypeAlias:
     """Get appropriate field types."""
     if isinstance(raw_response, dict):
         if "value" in raw_response:
@@ -43,7 +55,7 @@ def get_field_type(raw_response: StringDict, indent_level: int = 0) -> DynamicFi
             field_file = import_module("mindee.parsing.v2.field.object_field")
             field_class = getattr(field_file, FieldType.OBJECT.value)
         else:
-            raise MindeeApiV2Error(f"Unrecognized field format in {raw_response}.")
+            raise MindeeApiV2Error(f"Unrecognized field type in {raw_response}.")
         return field_class(raw_response, indent_level)
 
     raise MindeeApiV2Error(f"Unrecognized field format {raw_response}.")
