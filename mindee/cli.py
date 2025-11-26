c9d5b345d9d398e9cf2dd47962fc85b6b71172fc
ianaré sévi <ianare@mindee.co>
2025-11-26 15:15:22 +0100

    :coffin: remove unused feedback calls from CLI

--- mindee/cli.py
+++ mindee/cli.py
@@ -4,4 +4,4 @@ from mindee.commands.cli_parser import MindeeParser
 def main() -> None:
     """Run the Command Line Interface."""
     parser = MindeeParser()
-    parser.call_endpoint()
+    parser.call_parse()
