47532c3c7e1f4922bb1ae471565e42e722cd4988
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2025-11-28 12:13:56 +0100

    :recycle: add missing text_context active option return param

--- mindee/parsing/v2/inference_active_options.py
+++ mindee/parsing/v2/inference_active_options.py
@@ -5,15 +5,22 @@ class InferenceActiveOptions:
     """Active options for the inference."""
 
     raw_text: bool
+    """Whether raw text extraction is active or not."""
     polygon: bool
+    """Whether polygon extraction is active or not."""
     confidence: bool
+    """Whether confidence scores are active or not."""
     rag: bool
+    """Whether RAG is active or not."""
+    text_context: bool
+    """Whether text context is active or not."""
 
     def __init__(self, raw_response: StringDict):
         self.raw_text = raw_response["raw_text"]
         self.polygon = raw_response["polygon"]
         self.confidence = raw_response["confidence"]
         self.rag = raw_response["rag"]
+        self.text_context = raw_response["text_context"]
 
     def __str__(self) -> str:
         return (
