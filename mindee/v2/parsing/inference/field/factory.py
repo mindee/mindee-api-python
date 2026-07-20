5befe48bd3f3e81eaee1164c76b1c407f92d2c7e
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-07-20 14:56:14 +0200

    :recycle: update internals and bump dependencies (#451)

--- mindee/v2/parsing/inference/field/factory.py
+++ mindee/v2/parsing/inference/field/factory.py
@@ -5,13 +5,13 @@ from mindee.v2.parsing.inference.field.object_field import ObjectField
 from mindee.v2.parsing.inference.field.simple_field import SimpleField
 
 
-def parse_field(raw_response: StringDict, indent_level: int = 0):
+def create_field(raw_response: StringDict, indent_level: int = 0):
     """The central parser function to be injected down the tree."""
     if "value" in raw_response:
         return SimpleField(raw_response, indent_level)
     if "items" in raw_response:
-        return ListField(raw_response, parse_field, indent_level)
+        return ListField(raw_response, create_field, indent_level)
     if "fields" in raw_response:
-        return ObjectField(raw_response, parse_field, indent_level)
+        return ObjectField(raw_response, create_field, indent_level)
 
     raise MindeeAPIV2Error(f"Unrecognized field type in {raw_response}.")
