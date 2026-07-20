9930d0e7e679682ea424a66588248b19c7049aa3
sebastianMindee <130448732+sebastianMindee@users.noreply.github.com>
2026-07-20 16:41:42 +0200

    :sparkles: add webhook error response

---
+++ mindee/v2/parsing/failed_inference_response.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from mindee.parsing.common.common_response import CommonResponse
+from mindee.parsing.common.string_dict import StringDict
+from mindee.v2.parsing import ErrorResponse
+
+
+class FailedInferenceResponse(CommonResponse):
+    """Webhook payload returned when an inference fails before producing a result."""
+
+    inference_id: str
+    """UUID of the failed inference."""
+    model_id: str
+    """UUID of the model used."""
+    file_name: str
+    """Name of the input file."""
+    file_alias: str
+    """Alias sent for the file, if any."""
+    error: ErrorResponse
+    """Problem details for the failure, if available."""
+    created_at: datetime
+    """Date and time when the inference was started."""
+
+    def __init__(self, raw_prediction: StringDict) -> None:
+        super().__init__(raw_prediction)
+        self.inference_id = raw_prediction["inference_id"]
+        self.model_id = raw_prediction["model_id"]
+        self.file_name = raw_prediction["file_name"]
+        self.file_alias = raw_prediction["file_alias"]
+        self.error = ErrorResponse(raw_prediction["error"])
+        self.created_at = datetime.fromisoformat(raw_prediction["created_at"])
---
+++ tests/v2/parsing/test_failed_inference_response.py
@@ -0,0 +1,26 @@
+import json
+from datetime import datetime
+
+import pytest
+
+from mindee.v2.parsing.failed_inference_response import FailedInferenceResponse
+from tests.utils import V2_DATA_DIR
+
+
+@pytest.mark.v2
+def test_should_load_when_failed():
+    """Should load when the webhook didn't return a correct reply."""
+
+    json_path = V2_DATA_DIR / "errors" / "webhook_error_500_failed.json"
+    with json_path.open("r", encoding="utf-8") as fh:
+        json_sample = json.load(fh)
+    response = FailedInferenceResponse(json_sample)
+
+    assert response is not None
+    assert response.inference_id == "12345678-1234-1234-1234-123456789ABC"
+    assert response.file_name == "default_sample.jpg"
+    assert response.file_alias == "dummy-alias.jpg"
+    assert isinstance(response.created_at, datetime)
+    assert response.error is not None
+    assert response.error.status == 500
+    assert response.error.code == "500-012"
